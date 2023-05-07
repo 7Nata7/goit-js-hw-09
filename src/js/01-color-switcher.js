0bfa33b6b0484fc663b15c14bda54a8e1fd39fb6
Rubel' Nata <119401527+7Nata7@users.noreply.github.com>
2023-05-07 18:03:28 +0300

    task2done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let intervalId = null;
+
+function colorChange() {
+ startBtn.setAttribute('disabled', true);
+ stopBtn.removeAttribute('disabled');
+intervalId = setInterval(() => {
+     const color = getRandomHexColor();
+     bodyEl.style.backgroundColor = color;
+   }, 1000);
+}
+
+function colorStop() {
+ startBtn.removeAttribute('disabled');
+ stopBtn.setAttribute('disabled', true);
+ clearInterval(intervalId);
+}
+
+startBtn.addEventListener('click', colorChange);
+stopBtn.addEventListener('click', colorStop);
---
+++ src/js/02-timer.js
@@ -0,0 +1,84 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+const calendar = document.getElementById("datetime-picker");
+
+
+const refs = {
+ startBtn: document.querySelector("button[data-start]"),
+ daysSpan: document.querySelector("span[data-days]"),
+ hrsSpan: document.querySelector("span[data-hours]"),
+ minSpan: document.querySelector("span[data-minutes]"),
+ secSpan: document.querySelector("span[data-seconds]"),
+}
+
+refs.startBtn.setAttribute("disabled", "")
+
+let chosenDate;
+const options = {
+ enableTime: true,
+ time_24hr: true,
+ defaultDate: new Date(),
+ minuteIncrement: 1,
+ onClose(selectedDates) {
+   chosenDate = selectedDates[0];
+   if (chosenDate > new Date()) {
+    refs.startBtn.removeAttribute("disabled")
+   } else {
+    Notiflix.Notify.failure("Please choose a date in the future")
+   }
+ },
+};
+
+flatpickr(calendar, options)
+
+let timer;
+
+const handleTime = (targetDate) => {
+ const now = new Date()
+ const distance = targetDate - now;
+
+ if (distance <= 0) {
+  clearInterval(timer);
+  return;
+ }
+
+ function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+ }
+
+ const { days, hours, minutes, seconds } = convertMs(distance);
+
+ refs.daysSpan.textContent = addLeadingZero(days)
+ refs.hrsSpan.textContent = addLeadingZero(hours)
+ refs.minSpan.textContent = addLeadingZero(minutes)
+ refs.secSpan.textContent = addLeadingZero(seconds)
+}
+
+
+const startTimer = () => {
+ if (!chosenDate) {
+  return;
+ }
+
+ clearInterval(timer);
+ timer = setInterval(() => handleTime(chosenDate), 1000);
+};
+
+refs.startBtn.addEventListener("click", () => startTimer());
+
+ function convertMs(ms) {
+
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+ 
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+ 
+  return { days, hours, minutes, seconds };
+ }
