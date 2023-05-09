ba4301e8bbdb71849d19b70c79849d45de5f37db
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2023-05-09 16:51:03 +0000

    Deploying to gh-pages from @ 7Nata7/goit-js-hw-09@aff496f7a0ed82f9397e1673e7af68c8d4731d0d 🚀

--- 01-color-switcher.8fbde973.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");var a=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),a=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));r.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(a)}))}();
-//# sourceMappingURL=01-color-switcher.8fbde973.js.map
